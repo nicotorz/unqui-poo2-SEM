f90e65d2eaa083e3a2c21937b66a378947e13a3d
axlg98 <axlgarcia080498@gmail.com>
2024-05-25 03:01:19 -0300

    Avanzandp con SEMSystem

--- src/ar/edu/unq/po2/Sem/Clock.java
+++ src/ar/edu/unq/po2/Sem/Clock.java
@@ -1,5 +1,19 @@
 package ar.edu.unq.po2.Sem;
 
-public class Clock {
+import java.time.LocalDateTime;
 
+public class Clock {
+	private LocalDateTime currentTime;
+	
+	public Clock(LocalDateTime currentTime) {
+		this.setCurrentTime(currentTime);
+	}
+
+	public LocalDateTime getCurrentTime() {
+		return currentTime;
+	}
+
+	public void setCurrentTime(LocalDateTime currentTime) {
+		this.currentTime = currentTime;
+	}
 }
